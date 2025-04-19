c14c6d58cb78f08837d4eefe0c128bc4da2c2b32
KRISHNA666 <113873743+KRSHNA666@users.noreply.github.com>
2025-04-19 23:13:14 +0530

    Create deprecation.java

---
+++ deprecation.java
@@ -0,0 +1,9 @@
+@SuppressWarnings("deprecation")
+public class MyHouse implements House {
+    @Override
+    public void open() {}
+    @Override
+    public void openFrontDoor() {}
+    @Override
+    public void openBackDoor() {}
+}
